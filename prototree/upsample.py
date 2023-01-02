a2fbd6cc76eb25ddd62ee3a81be89d7ae49cfea4
siwen26 <62224198+siwen26@users.noreply.github.com>
2023-01-02 21:50:00 +0800

    Update upsample.py

--- prototree/upsample.py
+++ prototree/upsample.py
@@ -10,6 +10,15 @@ from torch.utils.data import DataLoader
 from prototree.prototree import ProtoTree
 from util.log import Log
 
+from util.net import BERT_EMBEDDING
+from transformers import BertTokenizer
+
+import re
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
 # adapted from protopnet
 def upsample(tree: ProtoTree, project_info: dict, project_loader: DataLoader, folder_name: str, args: argparse.Namespace, log: Log):
     dir = os.path.join(os.path.join(args.log_dir, args.dir_for_saving_images), folder_name)
@@ -18,67 +27,100 @@ def upsample(tree: ProtoTree, project_info: dict, project_loader: DataLoader, fo
     with torch.no_grad():
         sim_maps, project_info = get_similarity_maps(tree, project_info, log)
         log.log_message("\nUpsampling prototypes for visualization...")
-        imgs = project_loader.dataset.imgs
+        imgs = project_loader.dataset
         for node, j in tree._out_map.items():
             if node in tree.branches: #do not upsample when node is pruned
                 prototype_info = project_info[j]
                 decision_node_idx = prototype_info['node_ix']
-                x = Image.open(imgs[prototype_info['input_image_ix']][0])
-                x.save(os.path.join(dir,'%s_original_image.png'%str(decision_node_idx)))
+                x = imgs[prototype_info['input_image_ix']][0]
+#                 x.save(os.path.join(dir,'%s_original_image.png'%str(decision_node_idx)))
                     
-                x_np = np.asarray(x)
-                x_np = np.float32(x_np)/ 255
-                if x_np.ndim == 2: #convert grayscale to RGB
-                    x_np = np.stack((x_np,)*3, axis=-1)
+#                 x_np = np.asarray(x)
+#                 x_np = np.float32(x_np)/ 255
+#                 if x_np.ndim == 2: #convert grayscale to RGB
+#                     x_np = np.stack((x_np,)*3, axis=-1)
                 
-                img_size = x_np.shape[:2]
-                similarity_map = sim_maps[j]
+#                 img_size = x_np.shape[:2]
+#                 similarity_map = sim_maps[j]
 
-                rescaled_sim_map = similarity_map - np.amin(similarity_map)
-                rescaled_sim_map= rescaled_sim_map / np.amax(rescaled_sim_map)
-                similarity_heatmap = cv2.applyColorMap(np.uint8(255*rescaled_sim_map), cv2.COLORMAP_JET)
-                similarity_heatmap = np.float32(similarity_heatmap) / 255
-                similarity_heatmap = similarity_heatmap[...,::-1]
-                plt.imsave(fname=os.path.join(dir,'%s_heatmap_latent_similaritymap.png'%str(decision_node_idx)), arr=similarity_heatmap, vmin=0.0,vmax=1.0)
+#                 rescaled_sim_map = similarity_map - np.amin(similarity_map)
+#                 rescaled_sim_map= rescaled_sim_map / np.amax(rescaled_sim_map)
+#                 similarity_heatmap = cv2.applyColorMap(np.uint8(255*rescaled_sim_map), cv2.COLORMAP_JET)
+#                 similarity_heatmap = np.float32(similarity_heatmap) / 255
+#                 similarity_heatmap = similarity_heatmap[...,::-1]
+#                 plt.imsave(fname=os.path.join(dir,'%s_heatmap_latent_similaritymap.png'%str(decision_node_idx)), arr=similarity_heatmap, vmin=0.0,vmax=1.0)
 
-                upsampled_act_pattern = cv2.resize(similarity_map,
-                                                    dsize=(img_size[1], img_size[0]),
-                                                    interpolation=cv2.INTER_CUBIC)
-                rescaled_act_pattern = upsampled_act_pattern - np.amin(upsampled_act_pattern)
-                rescaled_act_pattern = rescaled_act_pattern / np.amax(rescaled_act_pattern)
-                heatmap = cv2.applyColorMap(np.uint8(255*rescaled_act_pattern), cv2.COLORMAP_JET)
-                heatmap = np.float32(heatmap) / 255
-                heatmap = heatmap[...,::-1]
+#                 upsampled_act_pattern = cv2.resize(similarity_map,
+#                                                     dsize=(img_size[1], img_size[0]),
+#                                                     interpolation=cv2.INTER_CUBIC)
+#                 rescaled_act_pattern = upsampled_act_pattern - np.amin(upsampled_act_pattern)
+#                 rescaled_act_pattern = rescaled_act_pattern / np.amax(rescaled_act_pattern)
+#                 heatmap = cv2.applyColorMap(np.uint8(255*rescaled_act_pattern), cv2.COLORMAP_JET)
+#                 heatmap = np.float32(heatmap) / 255
+#                 heatmap = heatmap[...,::-1]
                 
-                overlayed_original_img = 0.5 * x_np + 0.2 * heatmap
-                plt.imsave(fname=os.path.join(dir,'%s_heatmap_original_image.png'%str(decision_node_idx)), arr=overlayed_original_img, vmin=0.0,vmax=1.0)
+#                 overlayed_original_img = 0.5 * x_np + 0.2 * heatmap
+#                 plt.imsave(fname=os.path.join(dir,'%s_heatmap_original_image.png'%str(decision_node_idx)), arr=overlayed_original_img, vmin=0.0,vmax=1.0)
 
-                # save the highly activated patch
-                masked_similarity_map = np.zeros(similarity_map.shape)
-                prototype_index = prototype_info['patch_ix']
-                W, H = prototype_info['W'], prototype_info['H']
-                assert W == H
-                masked_similarity_map[prototype_index // W, prototype_index % W] = 1 #mask similarity map such that only the nearest patch z* is visualized
+#                 # save the highly activated patch
+#                 masked_similarity_map = np.zeros(similarity_map.shape)
+#                 prototype_index = prototype_info['patch_ix']
+#                 W, H = prototype_info['W'], prototype_info['H']
+#                 assert W == H
+#                 masked_similarity_map[prototype_index // W, prototype_index % W] = 1 #mask similarity map such that only the nearest patch z* is visualized
                 
-                upsampled_prototype_pattern = cv2.resize(masked_similarity_map,
-                                                    dsize=(img_size[1], img_size[0]),
-                                                    interpolation=cv2.INTER_CUBIC)
-                plt.imsave(fname=os.path.join(dir,'%s_masked_upsampled_heatmap.png'%str(decision_node_idx)), arr=upsampled_prototype_pattern, vmin=0.0,vmax=1.0) 
+#                 upsampled_prototype_pattern = cv2.resize(masked_similarity_map,
+#                                                     dsize=(img_size[1], img_size[0]),
+#                                                     interpolation=cv2.INTER_CUBIC)
+#                 plt.imsave(fname=os.path.join(dir,'%s_masked_upsampled_heatmap.png'%str(decision_node_idx)), arr=upsampled_prototype_pattern, vmin=0.0,vmax=1.0) 
                     
-                high_act_patch_indices = find_high_activation_crop(upsampled_prototype_pattern, args.upsample_threshold)
-                high_act_patch = x_np[high_act_patch_indices[0]:high_act_patch_indices[1],
-                                                    high_act_patch_indices[2]:high_act_patch_indices[3], :]
-                plt.imsave(fname=os.path.join(dir,'%s_nearest_patch_of_image.png'%str(decision_node_idx)), arr=high_act_patch, vmin=0.0,vmax=1.0)
+#                 high_act_patch_indices = find_high_activation_crop(upsampled_prototype_pattern, args.upsample_threshold)
+#                 high_act_patch = x_np[high_act_patch_indices[0]:high_act_patch_indices[1],
+#                                                     high_act_patch_indices[2]:high_act_patch_indices[3], :]
+#                 plt.imsave(fname=os.path.join(dir,'%s_nearest_patch_of_image.png'%str(decision_node_idx)), arr=high_act_patch, vmin=0.0,vmax=1.0)
 
-                # save the original image with bounding box showing high activation patch
-                imsave_with_bbox(fname=os.path.join(dir,'%s_bounding_box_nearest_patch_of_image.png'%str(decision_node_idx)),
-                                    img_rgb=x_np,
-                                    bbox_height_start=high_act_patch_indices[0],
-                                    bbox_height_end=high_act_patch_indices[1],
-                                    bbox_width_start=high_act_patch_indices[2],
-                                    bbox_width_end=high_act_patch_indices[3], color=(0, 255, 255))
+#                 # save the original image with bounding box showing high activation patch
+#                 imsave_with_bbox(fname=os.path.join(dir,'%s_bounding_box_nearest_patch_of_image.png'%str(decision_node_idx)),
+#                                     img_rgb=x_np,
+#                                     bbox_height_start=high_act_patch_indices[0],
+#                                     bbox_height_end=high_act_patch_indices[1],
+#                                     bbox_width_start=high_act_patch_indices[2],
+#                                     bbox_width_end=high_act_patch_indices[3], color=(0, 255, 255))
+                  draw_word_embedding_fugure(x, decision_node_idx)
+    
     return project_info
 
+
+def draw_word_embedding_figure(word_input_ids, decision_node_idx):
+        model = BERT_EMBEDDING.from_pretrained("bert-base-cased")
+        output, _ = model(word_input_ids)
+        tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=True)
+        token_label = tokenizer.convert_ids_to_tokens(word_input_ids.tolist()[0])
+        np_output = output.detach().to('cpu').numpy()
+        token_embedding = np_output.reshape(np_output.shape[1], -1)
+        
+        tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+        new_values = tsne_model.fit_transform(token_embedding)
+        
+        x = []
+        y = []
+        
+        for value in new_values:
+            x.append(value[0])
+            y.append(value[1])
+        
+        plt.figure(figsize=(8,8)) 
+        for i in range(len(x)):
+            plt.scatter(x[i],y[i])
+            plt.annotate(token_labels[i],
+                         xy=(x[i], y[i]),
+                         xytext=(5, 2),
+                         textcoords='offset points',
+                         ha='right',
+                         va='bottom')
+        plt.savefig('%s_bert_embedding_tsne_image.png'%str(decision_node_idx))
+        
+        
 
 def get_similarity_maps(tree: ProtoTree, project_info: dict, log: Log = None):
     log.log_message("\nCalculating similarity maps (after projection)...")
