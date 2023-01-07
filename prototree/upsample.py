cfd178b1bf3ab1a4cca991d4a0bb953a0f8c2e84
siwen26 <62224198+siwen26@users.noreply.github.com>
2023-01-08 00:14:30 +0800

    Update upsample.py

--- prototree/upsample.py
+++ prototree/upsample.py
@@ -15,8 +15,11 @@ from transformers import BertTokenizer
 
 import re
 from sklearn.manifold import TSNE
+import matplotlib
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 
 
 
@@ -25,18 +28,23 @@ def upsample(tree: ProtoTree, project_info: dict, project_loader: DataLoader, fo
     dir = os.path.join(os.path.join(args.log_dir, args.dir_for_saving_images), folder_name)
     if not os.path.exists(dir):
         os.makedirs(dir)
+    tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=True)
+
     with torch.no_grad():
         sim_maps, project_info, attn_maps = get_similarity_maps(tree, project_info)
-#         log.log_message("\nUpsampling prototypes for visualization...")
+        # log.log_message("\nUpsampling prototypes for visualization...")
         imgs = project_loader.dataset
         for node, j in tree._out_map.items():
             if node in tree.branches: #do not upsample when node is pruned
                 prototype_info = project_info[j]
                 decision_node_idx = prototype_info['node_ix']
                 x = imgs[prototype_info['input_image_ix']][0]
+                token_labels = tokenizer.convert_ids_to_tokens(x.tolist()[0])
+
                 attentions_array = attn_maps[j]
-                fname=os.path.join(dir, '%s_bert_embedding_tsne_image.png'%str(decision_node_idx))
-                draw_attention_map(attentions_array, fname)
+                
+                fname=os.path.join(dir, '%s_bert_embedding_attention_image.png'%str(decision_node_idx))
+                draw_attention_map(attentions_array, token_labels, fname)
                 
                 
 #                 x.save(os.path.join(dir,'%s_original_image.png'%str(decision_node_idx)))
@@ -127,21 +135,142 @@ def draw_word_embedding_figure(word_input_ids, decision_node_idx):
         plt.savefig('%s_bert_embedding_tsne_image.png'%str(decision_node_idx))
 
   
-def draw_attention_map(array_attention, filename):
-      plt.rcParams['figure.figsize'] = (10, 5)
-      color_palette = sns.diverging_palette(250, 0, as_cmap=True)
-      heatmap = sns.heatmap(array_attention,
-                cmap=color_palette,
-                center=0,
-                vmin=0,
-                vmax=1,
-               );
-      heatmap.figure.savefig(filename)
+def draw_attention_map(array_attention, token_labels, filename):
+      fig, ax = plt.subplots()
+      plt.rcParams['figure.figsize'] = (8, 5)
+      im, cbar = heatmap(array_attention, token_labels, ax=ax,
+                   cmap="YlGn", cbarlabel="Attention Weights")
+      texts = annotate_heatmap(im, valfmt="{x:.3f}")
+      fig.tight_layout()
+      plt.savefig(filename)
+      
+
+
+def heatmap(data, row_labels, ax=None,
+            cbar_kw=None, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (M, N).
+    row_labels
+        A list or array of length M with the labels for the rows.
+    col_labels
+        A list or array of length N with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if ax is None:
+        ax = plt.gca()
+
+    if cbar_kw is None:
+        cbar_kw = {}
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # Show all ticks and label them with the respective list entries.
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+
+    ax.set_xticklabels(row_labels) 
+    ax.set_yticklabels(row_labels) 
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    # ax.spines[:].set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=("black", "white"),
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
         
         
 
 def get_similarity_maps(tree: ProtoTree, project_info: dict):
-#     log.log_message("\nCalculating similarity maps (after projection)...")
+    # log.log_message("\nCalculating similarity maps (after projection)...")
     
     sim_maps = dict()
     attn_maps = dict()
